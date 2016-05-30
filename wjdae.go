d0bafebe050bd479aea7a0474fce1e7e090520c9
cjlibra <cjlibra@qq.com>
2016-05-30 16:37:13 +0800

    开头

---
+++ wjdae.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/golang/glog"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func db() {
+	session, err := mgo.Dial("202.127.26.247")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	//session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	/*	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+			&Person{"Cla", "+55 53 8402 8510"})
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	//result := Person{}
+	var result1 []Person
+	err = c.Find(bson.M{"name": "Ale"}).All(&result1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, value := range result1 {
+		fmt.Println("Phone:", value.Phone, i)
+	}
+}
+
+func SocketServer(sockport string) {
+
+	netListen, err := net.Listen("tcp", ":"+sockport)
+	CheckError(err)
+
+	defer netListen.Close()
+
+	Log("后台服务启动于端口 " + sockport)
+	for {
+		conn, err := netListen.Accept()
+		if err != nil {
+			continue
+		}
+
+		Log(conn.RemoteAddr().String(), " tcp connect success")
+		go handleConnection(conn)
+	}
+
+}
+
+func main() {
+	sockport := flag.Int("p", 48080, "socket server port")
+	webport := flag.Int("wp", 58080, "socket server port")
+	flag.Parse()
+
+	go SocketServer(fmt.Sprintf("%d", *sockport))
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./htmlsrc/"))))
+
+	glog.Info("WEB程序启动，开始监听" + fmt.Sprintf("%d", *webport) + "端口")
+	err := http.ListenAndServe(":"+fmt.Sprintf("%d", *webport), nil)
+	if err != nil {
+
+		glog.Info("ListenAndServer: ", err)
+
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			Log(conn.RemoteAddr().String(), "连接出错: ", err)
+			return
+		}
+		Log(conn.RemoteAddr().String(), "receive data length:", n)
+		Log(conn.RemoteAddr().String(), "receive data:", buffer[:n])
+		Log(conn.RemoteAddr().String(), "receive data string:", string(buffer[:n]))
+	}
+}
+
+func Log(v ...interface{}) {
+	glog.Info(v...)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
