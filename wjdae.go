84575a427e20e7c2d91e3cbbf564cd7cfbfebe7e
cjlibra <cjlibra@qq.com>
2016-07-28 09:35:33 +0800

    冒号不行，只能用-，空格用$

--- wjdae.go
+++ wjdae.go
@@ -1220,7 +1220,8 @@ func UploadFiletoServer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	FirmFileOnServerDir := "./upload/" + handle.Filename + time.Now().Local().String()
-	FirmFileOnServerDir = strings.Replace(FirmFileOnServerDir, ":", "：", -1)
+	FirmFileOnServerDir = strings.Replace(FirmFileOnServerDir, ":", "-", -1)
+	FirmFileOnServerDir = strings.Replace(FirmFileOnServerDir, " ", "$", -1)
 	f, err := os.OpenFile(FirmFileOnServerDir, os.O_WRONLY|os.O_CREATE, 0666)
 	io.Copy(f, file)
 	if err != nil {
