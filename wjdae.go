5263fae8f7c72ab74cdbec0645b1134ed7100bfc
cjlibra <cjlibra@qq.com>
2016-08-09 16:53:25 +0800

    第二期开发开始，实现标签数据接收

--- wjdae.go
+++ wjdae.go
@@ -1688,6 +1688,88 @@ func isfoundserialinpool(buffer []byte) int {
 	}
 	return -1
 }
+func crc8(cmdBuf []byte) byte {
+	bufLen := len(cmdBuf)
+	CRC8_Table := []byte{
+		0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+		157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+		35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+		190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+		70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+		219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+		101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+		248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+		140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+		17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+		175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+		50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+		202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+		87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+		233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+		116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53}
+	var crc8 byte = 0
+
+	for i := 0; bufLen > 0; bufLen-- {
+		crc8 = CRC8_Table[crc8^cmdBuf[0]]
+		i++ //查表得到CRC码
+		cmdBuf = cmdBuf[i:]
+	}
+
+	return crc8
+}
+
+type DATAINFO struct {
+	ipstring   string
+	dotime     time.Time
+	labelinfos []LABELINFO
+}
+type LABELINFO struct {
+	labelid string
+	rssi    string
+}
+
+func dealwithdata(buffer []byte, ipstring string) {
+	lenofbuf := len(buffer)
+	if lenofbuf <= 3 {
+		glog.V(5).Infoln("数据包长度太短，不对")
+		return
+	}
+	if buffer[0] != 0xaa || buffer[lenofbuf-1] != 0xee || buffer[1] != 0x97 {
+		glog.V(5).Infoln("数据包格式不对")
+		return
+	}
+	tmpcrc8 := crc8(buffer[0 : lenofbuf-2])
+	if tmpcrc8 != buffer[lenofbuf-2] {
+		glog.V(5).Infoln("数据包校验码不对")
+		return
+	}
+	countoflabel := int(buffer[8])
+	if countoflabel <= 0 {
+		glog.V(5).Infoln("数据包标签数为小于等于0")
+		return
+	}
+	var labelinfo LABELINFO
+	var labelinfos []LABELINFO
+	for i := 0; i < countoflabel; i++ {
+		labelinfo.labelid = hex.EncodeToString(buffer[9+5*i : 9+4+5*i])
+		labelinfo.rssi = hex.EncodeToString(buffer[9+4+5*i : 9+4+5*i+1])
+		labelinfos = append(labelinfos, labelinfo)
+	}
+	var sdatainfo DATAINFO
+	sdatainfo.dotime = time.Now().Local()
+	sdatainfo.ipstring = ipstring
+	sdatainfo.labelinfos = labelinfos
+
+	cddb := session.DB("data").C("info")
+	err := cddb.Insert(&sdatainfo)
+	if err != nil {
+
+		glog.V(1).Infoln("data插入数据库有问题")
+		return
+	}
+	glog.V(2).Infoln("data插入数据库成功")
+
+}
 func handleConnection(conn net.Conn) {
 	var onelineinfo CONNINFO
 	defer func() {
@@ -1711,6 +1793,8 @@ func handleConnection(conn net.Conn) {
 		}
 
 		glog.V(5).Infoln(conn.RemoteAddr().String(), "->", hex.EncodeToString(buffer[:n]), n)
+
+		dealwithdata(buffer, conn.RemoteAddr().String())
 
 		ret := isfoundserialinpool(buffer)
 		if ret == -1 {
